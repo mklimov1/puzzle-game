568732bdd05782b785fb85cf6fd04148b495f3b4
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2023-01-30 18:15:53 +0300

    added assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,18 @@
+import card0 from './cards/0.png';
+import card1 from './cards/1.png';
+import card2 from './cards/2.png';
+import card3 from './cards/3.png';
+import card4 from './cards/4.png';
+import card5 from './cards/5.png';
+
+export const images = {
+  cards: {
+    0: card0,
+    1: card1,
+    2: card2,
+    3: card3,
+    4: card4,
+    5: card5,
+  },
+
+};
